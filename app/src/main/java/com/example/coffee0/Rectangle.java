16c6166d0e5a48be2dc1043f13c8285ce7185fad
Harsh Gill <harsh.gill@yahoo.com>
2024-09-26 18:09:08 -0600

    Added Triangle as rectangle is taken

--- app/src/main/java/com/example/coffee0/Rectangle.java
+++
@@ -1,7 +0,0 @@
-package com.example.coffee0;
-
-public class Rectangle extends Shape{
-    public Rectangle() {
-        super();
-    }
-}
---
+++ app/src/main/java/com/example/coffee0/Triangle.java
@@ -0,0 +1,7 @@
+package com.example.coffee0;
+
+public class Triangle extends Shape{
+    public Triangle() {
+        super();
+    }
+}
